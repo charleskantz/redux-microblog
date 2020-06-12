0b44d1e82dad2495723ff322315aab7c94ee1b3e
Charles Kantz <kantz.charles@gmail.com>
2020-06-11 23:53:43 -0700

    API address fix

--- src/MicroblogAPI.js
+++ src/MicroblogAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://ck-microblog.herokuapp.com/api/posts/';
+const BASE_URL = 'https://ck-microblog.herokuapp.com/api/posts';
 
 class MicroBlogAPI {
   static async getTitles() {
